fd5246f173f417afa28c4caf50cd782597096cc8
loordbarringtn <quantas@gmail.com>
2022-12-09 01:45:34 +0300

    first commit

---
+++ goods.js
@@ -0,0 +1,7 @@
+const productName = 'Apple';
+const goodsQuantaty = 5;
+const productCategory = 'fruits';
+const productPrice = 2.55;
+
+console.log('Product type is: ' + productCategory);
+console.log('Total sum: ' +goodsQuantaty * productPrice);
