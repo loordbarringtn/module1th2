ddb6fc7d6370a52ffea01841abcbabdbd26c4c97
loordbarringtn <quantas@gmail.com>
2022-12-09 01:55:02 +0300

    second commit

--- goods.js
+++ goods.js
@@ -5,3 +5,11 @@ const productPrice = 2.55;
 
 console.log('Product type is: ' + productCategory);
 console.log('Total sum: ' +goodsQuantaty * productPrice);
+
+
+{
+    const productName = 'Arabica';
+    const goodsQuantaty = 2;
+    const productCategory = 'coffee';
+    const productPrice = 3.48;
+}
