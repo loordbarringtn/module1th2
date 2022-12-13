d9e7699023c2fe4b791603eae6f7d171962e9a50
loordbarringtn <quantas@gmail.com>
2022-12-14 00:22:11 +0300

    module3

--- goods.js
+++ goods.js
@@ -13,3 +13,16 @@ console.log('Total sum: ' +goodsQuantaty * productPrice);
     const productCategory = 'coffee';
     const productPrice = 3.48;
 }
+
+const productNameFromInput = prompt('Please type product name');
+const goodsQuantatyFromInput = prompt('Please type product quantaty');
+const productCategoryFromInput = prompt('Please type product category');
+const productPriceFromInput = prompt('Please type product price');
+
+console.log('productName:', typeof productNameFromInput);
+console.log('goodsQuantaty:', typeof goodsQuantatyFromInput);
+console.log('productCategory:', typeof productCategoryFromInput);
+console.log('productPrice:', typeof productPriceFromInput);
+
+console.log(`We have ${goodsQuantatyFromInput} of ${productNameFromInput} on a total sum: ${goodsQuantatyFromInput*productPriceFromInput} in our warehouse`);
+
---
+++ module3/einstein.js
@@ -0,0 +1,4 @@
+const myWeight = 12;
+const speedLight = 3e8;
+
+console.log(myWeight * Math.pow(speedLight, 2));
