dd6577985780faefdba9dda6fba7ac18b901c883
loordbarringtn <quantas@gmail.com>
2022-12-14 21:51:09 +0300

    refactoring

--- module3/goods.js
+++ module3/goods.js
@@ -1,15 +1,32 @@
-const productName = 'Apple';
-const goodsQuantaty = 5;
-const productCategory = 'fruits';
-const productPrice = 2.55;
+{
+  const productName = "Apple";
+  const goodsQuantity = 5;
+  const productCategory = "fruits";
+  const productPrice = 2.55;
 
-console.log('Product type is: ' + productCategory);
-console.log('Total sum: ' +goodsQuantaty * productPrice);
-
+  console.log("Product type is: " + productCategory);
+  console.log("Total sum: " + goodsQuantity * productPrice);
+}
 
 {
-    const productName = 'Arabica';
-    const goodsQuantaty = 2;
-    const productCategory = 'coffee';
-    const productPrice = 3.48;
+  const productName = "Arabica";
+  const goodsQuantity = 2;
+  const productCategory = "coffee";
+  const productPrice = 3.48;
 }
+
+const productName = prompt("Please type product name");
+const goodsQuantity = prompt("Please type product Quantity");
+const productCategory = prompt("Please type product category");
+const productPrice = prompt("Please type product price");
+
+console.log("productName:", typeof productName);
+console.log("goodsQuantity:", typeof goodsQuantity);
+console.log("productCategory:", typeof productCategory);
+console.log("productPrice:", typeof productPrice);
+
+console.log(
+  `We have ${goodsQuantity} of ${productName} on a total sum: ${
+    goodsQuantity * productPrice
+  } in our warehouse`
+);
---
+++ module3/module2/goods.js
@@ -0,0 +1,15 @@
+const productName = 'Apple';
+const goodsQuantaty = 5;
+const productCategory = 'fruits';
+const productPrice = 2.55;
+
+console.log('Product type is: ' + productCategory);
+console.log('Total sum: ' +goodsQuantaty * productPrice);
+
+
+{
+    const productName = 'Arabica';
+    const goodsQuantaty = 2;
+    const productCategory = 'coffee';
+    const productPrice = 3.48;
+}
